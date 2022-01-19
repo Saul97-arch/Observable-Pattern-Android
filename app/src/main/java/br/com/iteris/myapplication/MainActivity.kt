f6cd9078a44524d7687facc98ffe12a84d0c3ea5
Saul97-arch <duart.herrera@gmail.com>
2022-01-19 06:39:40 -0300

    Dark theme functional and some arragements in layout

--- app/src/main/java/br/com/iteris/myapplication/MainActivity.kt
+++ app/src/main/java/br/com/iteris/myapplication/MainActivity.kt
@@ -2,28 +2,43 @@ package br.com.iteris.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.darkTheme)
+        } else {
+            setTheme(R.style.Theme_MyApplication)
+        }
 
         val firstFragment = FirstFragment()
         val secondFragment = SecondFragment()
         val thirdFragment = ThirdFragment()
-        // SEMPRE QUE USAR FRAGMENT USE UM FRAMELAYOUT
-        /*
-        *  Strategy
-        *
-        * */
+
+        thirdFragment.register(firstFragment)
+        thirdFragment.register(secondFragment)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val switch = findViewById<SwitchCompat>(R.id.switch_theme)
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
         supportFragmentManager.beginTransaction().add(R.id.first_fragment, firstFragment)
             .commit()
         supportFragmentManager.beginTransaction().add(R.id.second_fragment, secondFragment)
             .commit()
         supportFragmentManager.beginTransaction().add(R.id.third_fragment, thirdFragment)
             .commit()
-
-        thirdFragment.register(firstFragment)
-        thirdFragment.register(secondFragment)
     }
 }
